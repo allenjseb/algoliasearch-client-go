d4e456f0413f2a1c29ff4441a84235c198ff2fa5
Anthony Seure <anthony.seure@gmail.com>
2020-02-18 18:00:25 +0100

    feat: introduce the new recommendation.Client for Personalization features
    Close #565

---
+++ algolia/recommendation/client.go
@@ -0,0 +1,54 @@
+package recommendation
+
+import (
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/call"
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/compression"
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/region"
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/transport"
+)
+
+// Client provides methods to interact with the Algolia Recommendation API.
+type Client struct {
+	appID     string
+	transport *transport.Transport
+}
+
+// NewClient instantiates a new client able to interact with the Algolia
+// Recommendation API.
+func NewClient(appID, apiKey string, region region.Region) *Client {
+	return NewClientWithConfig(
+		Configuration{
+			AppID:  appID,
+			APIKey: apiKey,
+			Region: region,
+		},
+	)
+}
+
+// NewClientWithConfig instantiates a new client able to interact with the
+// Recommendation API.
+func NewClientWithConfig(config Configuration) *Client {
+	var hosts []*transport.StatefulHost
+
+	if config.Hosts == nil {
+		hosts = defaultHosts(config.Region)
+	} else {
+		for _, h := range config.Hosts {
+			hosts = append(hosts, transport.NewStatefulHost(h, call.IsReadWrite))
+		}
+	}
+
+	return &Client{
+		transport: transport.New(
+			hosts,
+			config.Requester,
+			config.AppID,
+			config.APIKey,
+			config.ReadTimeout,
+			config.WriteTimeout,
+			config.Headers,
+			config.ExtraUserAgent,
+			compression.None,
+		),
+	}
+}
